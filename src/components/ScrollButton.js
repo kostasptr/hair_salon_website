f08545e963d28485fc41890beb93f68287f4d276
kostasptr <kostas-ptr@hotmail.com>
2021-09-04 21:26:29 +0300

    scroll button component created

---
+++ src/components/ScrollButton.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './ScrollButton.css';
+import buttonImage from '../images/up_button.jpg';
+
+const ScrollButton = () => {
+
+    const [visible, setVisible] = useState(false)
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 70) { setVisible(true) }
+        else if (scrolled <= 70) { setVisible(false) }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0, behavior: 'smooth'
+            /* you can also use 'auto' behaviour
+               in place of 'smooth' */});
+    };
+
+    window.addEventListener('scroll', toggleVisible);
+
+    return (
+        // <Button>
+        //     <FaArrowCircleUp onClick={scrollToTop}
+        //         style={{ display: visible ? 'inline' : 'none' }} />
+        // </Button>
+
+        <button className="btnContainer" onClick={scrollToTop}
+            style={{ display: visible ? 'inline' : 'none' }}>
+            <img src={buttonImage} alt="" className="btnImage" />
+        </button>
+    );
+}
+
+export default ScrollButton;
