39f545e2d90d2f9e491ec336dcfcce0d571cc1bb
kostasptr <kostas-ptr@hotmail.com>
2021-09-17 10:30:42 +0300

    empty spaces deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Routing, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Routing, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Products from './components/Products';
@@ -12,26 +12,27 @@ import History from './components/History';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
+
 function App() {
   return (
 
     <Routing>
-      <div className="App"> 
-        <Header/>
+      <div className="App">
+        <Header />
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/home" exact component={Home}/>
-          <Route path="/hair" component={Hair}/>
-          <Route path="/cafe" component={Cafe}/>
-          <Route path="/products" component={Products}/>
-          <Route path="/book" component={Book}/>
-          <Route path="/history" component={History}/>
-          <Route path="/contact" component={Contact}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/hair" component={Hair} />
+          <Route path="/cafe" component={Cafe} />
+          <Route path="/products" component={Products} />
+          <Route path="/book" component={Book} />
+          <Route path="/history" component={History} />
+          <Route path="/contact" component={Contact} />
         </Switch>
-        <Footer/>
+        <Footer />
       </div>
     </Routing>
-    
+
   );
 }
 
