4827724ed723be374f6a3bcaf5e6867783b85d59
Kostas Petrou <kostas-ptr@hotmail.com>
2021-10-11 13:55:02 +0300

    class for fotters' inputs added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,66 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logo from '../images/logo3.jpg';
 
 function Header() {
+  const [isHidden, setHidden] = useState("true");
 
-  function myFunction() {
-    const HEIGHTNEW = document.querySelector(".flex-header");
-    const TOGGLEMENU1 = document.querySelector(".order1");
-    const TOGGLEMENU3 = document.querySelector(".order3");
-    if (HEIGHTNEW.style.height === "70px") {
-      HEIGHTNEW.style.height = "auto";
-      TOGGLEMENU1.style.display = "flex";
-      TOGGLEMENU3.style.display = "flex";
-    }
-    else {
-      HEIGHTNEW.style.height = "70px";
-      TOGGLEMENU1.style.display = "none";
-      TOGGLEMENU3.style.display = "none";
-    }
-  }
+  function toggleHidden() {
+    setHidden(!isHidden);
+  };
 
   return (
-    <div className="App-Header">
-      <header>
-        <nav className="flex-header">
+    <header className="App-Header">
+      <nav className="flex-header">
 
-          <div role="navigation" aria-label="" className="nav-menu order1">
-            <ul className="nav-items">
-              <li className="nav-link"><a href="/"> Home </a></li>
-              <li className="nav-link"><a href="Hair"> Hair </a></li>
-              <li className="nav-link"><a href="Products"> Products We Use </a></li>
-              <li className="nav-link"><a href="Cafe"> Cafe </a></li>
-              <li className="nav-link"><a href="History"> Our History </a></li>
-            </ul>
-          </div>
+        <div role="navigation" aria-label="menu 1" className="nav-menu tablet">
+          <ul className="nav-items">
+            <li className="nav-link"><a href="/"> Home </a></li>
+            <li className="nav-link"><a href="Hair"> Hair </a></li>
+            <li className="nav-link"><a href="Products"> Products We Use </a></li>
+            <li className="nav-link"><a href="Cafe"> Cafe </a></li>
+            <li className="nav-link"><a href="History"> Our History </a></li>
+          </ul>
+        </div>
 
-          <div className="brand order2">
-            <picture>
-              <a href="/"><img className="logoWest" src={logo} alt="john west icon" /></a>
-            </picture>
-          </div>
+        <a className="brand" href="/">
+          {/* <a href="/"> */}
+          <img className="logoWest" src={logo} alt="john west icon" />
+          {/* </a> */}
+        </a>
 
-          <div role="navigation" aria-label="Customer service" className="nav-menu order3">
-            <ul className="nav-items bottom-space">
-              <li className="nav-link">+30 6972827033</li>
-              <li className="nav-link"><a href="Email"> Email Us </a></li>
-              <li className="nav-link"><a href="Book"> Book Online </a></li>
-              <li className="nav-link"><a href="Contact"> Contact </a></li>
-            </ul>
-          </div>
+        <div role="navigation" aria-label="menu 2" className="nav-menu tablet">
+          <ul className="nav-items bottom-space">
+            <li className="nav-link">+30 6972827033</li>
+            <li className="nav-link"><a href="Email"> Email Us </a></li>
+            <li className="nav-link"><a href="Book"> Book Online </a></li>
+            <li className="nav-link"><a href="Contact"> Contact </a></li>
+          </ul>
+        </div>
 
-          <div className="dropdown order4">
-            <button onClick={myFunction} className="toggle-icons dropbtn">
-              <span><FontAwesomeIcon icon={faBars} /></span>
-            </button>
-          </div>
+        <div className="dropdown">
+          <button onClick={toggleHidden} className="toggle-icons dropbtn">
+            <span><FontAwesomeIcon icon={faBars} /></span>
+          </button>
+        </div>
 
-        </nav>
-      </header>
-    </div>
+        <div role="navigation" aria-label="menu 1" className={'nav-menu mobile' + (isHidden ? ' hidden' : '')}>
+          <ul className="nav-items">
+            <li className="nav-link"><a href="/"> Home </a></li>
+            <li className="nav-link"><a href="Hair"> Hair </a></li>
+            <li className="nav-link"><a href="Products"> Products We Use </a></li>
+            <li className="nav-link"><a href="Cafe"> Cafe </a></li>
+            <li className="nav-link"><a href="History"> Our History </a></li>
+            <li className="nav-link">+30 6972827033</li>
+            <li className="nav-link"><a href="Email"> Email Us </a></li>
+            <li className="nav-link"><a href="Book"> Book Online </a></li>
+            <li className="nav-link"><a href="Contact"> Contact </a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 }
 export default Header;
