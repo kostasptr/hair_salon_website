ce92662721c3bda9972fb0fd1240e7e474b3da1e
kostasptr <kostas-ptr@hotmail.com>
2021-10-12 13:10:01 +0300

    updated adress

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
       <div className="flex-footer">
 
         <div>
-          <p>170 Crawford Street <br></br>London<br></br>W1H2JQ<br></br>United Kingdom<br></br></p>
+          <p>83-109 Seymour Place <br></br>London<br></br>W1H 4PB<br></br>United Kingdom<br></br></p>
           <p>(44) 207205-2555<br></br>info@amadeus.com</p>
           <p><strong>Follow us</strong><br></br>
             <FontAwesomeIcon icon={faFacebookF} style={{ color: "black" }} size="sm" />&nbsp;
