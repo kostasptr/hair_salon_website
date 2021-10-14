5aa916cadb41df2681c5ce376495c5ce61e4eaa9
kostasptr <kostas-ptr@hotmail.com>
2021-10-14 12:44:53 +0300

    fixed header for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,65 +1,84 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
-import './Header.css';
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logo from '../images/logo3.jpg';
+import './Header.css';
 
-function Header() {
-  const [isHidden, setHidden] = useState("true");
+class Header extends React.Component {
+  constructor(props){
+    super(props);
+    this.myRef = React.createRef();
+    this.myNav = React.createRef();
+    this.myNav2 = React.createRef();
 
-  function toggleHidden() {
-    setHidden(!isHidden);
-  };
+    this.openMenu = false;
+    this.showNav = true;
+    this.showNav2 = true;
+  }
+  
+  render(){
 
-  return (
+    const activateMenu = () => {
+      this.myRef.current.style.height = this.openMenu ? "35px" : "240px";
+      this.openMenu = !this.openMenu;
+      this.myNav.current.style.display = this.showNav ? "block" : "none";
+      this.showNav = !this.showNav;
+      this.myNav2.current.style.display = this.showNav2 ? "block" : "none";
+      this.showNav2 = !this.showNav2;
+      // if( this.myRef.current.style.height < "100px"){
+      //   this.myRef.current.style.height = "500px";
+      // }
+      // else {
+      //   this.myRef.current.style.height = "33px";
+      // }
+
+      // var x = this.myRef("flex-header");
+      
+      // if (x.current.className === "flex-header"){
+      //   x.current.className += "responsive";
+        
+      // } else {
+      //   x.current.className = "flex-header";
+        
+      // }
+    };
+
+    return (
+    
     <header className="App-Header">
-      <nav className="flex-header">
-
-        <div role="navigation" aria-label="menu 1" className="nav-menu tablet">
-          <ul className="nav-items">
-            <li className="nav-link"><Link to="/"> Home </Link></li>
-            <li className="nav-link"><Link to="hair"> Hair </Link></li>
-            <li className="nav-link"><Link to="products"> Products We Use </Link></li>
-            <li className="nav-link"><Link to="history"> Our History </Link></li>
-            <li className="nav-link"><Link to="cafe"> Cafe </Link></li>
-          </ul>
-        </div>
-
-        <a className="brand" href="/">
-          {/* <a href="/"> */}
+    <nav className="flex-header responsive" ref={this.myRef}>
+    
+      <div>
+        <a href="/">
           <img className="logoWest" src={logo} alt="john west icon" />
-          {/* </a> */}
         </a>
-
-        <div role="navigation" aria-label="menu 2" className="nav-menu tablet">
-          <ul className="nav-items bottom-space">
-            <li className="nav-link">+30 6972827033</li>
-            <li className="nav-link"><Link to="book"> Book Online </Link></li>
-            <li className="nav-link"><Link to="contact"> Contact </Link></li>
-          </ul>
-        </div>
-
-        <div className="dropdown">
-          <button onClick={toggleHidden} className="toggle-icons dropbtn">
-            <span><FontAwesomeIcon icon={faBars} /></span>
-          </button>
-        </div>
-
-        <div role="navigation" aria-label="menu 1" className={'nav-menu mobile' + (isHidden ? ' hidden' : '')}>
-          <ul className="nav-items">
-            <li className="nav-link"><Link to="/"> Home </Link></li>
-            <li className="nav-link"><Link to="hair"> Hair </Link></li>
-            <li className="nav-link"><Link to="products"> Products We Use </Link></li>
-            <li className="nav-link"><Link to="history"> Our History </Link></li>
-            <li className="nav-link"><Link to="cafe"> Cafe </Link></li>
-            <li className="nav-link">+30 6972827033</li>
-            <li className="nav-link"><Link to="book"> Book Online </Link></li>
-            <li className="nav-link"><Link to="contact"> Contact </Link></li>
-          </ul>
-        </div>
-      </nav>
+      </div>
+      <div className="navigationMobile" ref={this.myNav}>
+        <ul className="nav-items">
+          <li className="nav-link"><Link to="/"> Home </Link></li>
+          <li className="nav-link"><Link to="hair"> Hair </Link></li>
+          <li className="nav-link"><Link to="products"> Products We Use </Link></li>
+          <li className="nav-link"><Link to="history"> Our History </Link></li>
+          <li className="nav-link"><Link to="cafe"> Cafe </Link></li>
+        </ul>
+      </div>
+      <div className="navigationMobile" ref={this.myNav2}>
+        <ul className="nav-items">
+          <li className="nav-link">+30 6972827033</li>
+          <li className="nav-link"><Link to="book"> Book Online </Link></li>
+          <li className="nav-link"><Link to="contact"> Contact </Link></li>
+        </ul>
+      </div>
+      <div className="dropdown">
+        <button onClick={activateMenu}>
+          <span><FontAwesomeIcon icon={faBars} /></span>
+        </button>
+      </div>
+    </nav>
     </header>
-  );
+
+    )
+  }
 }
 export default Header;
