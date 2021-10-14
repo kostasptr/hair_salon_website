7df44b24148def37f0c1308bf074153bf20efd70
kostasptr <kostas-ptr@hotmail.com>
2021-10-14 17:12:35 +0300

    breakpoints added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-template-curly-in-string */
 import React from 'react';
 import PageBlock from './PageBlock';
 
@@ -6,43 +5,55 @@ import PageBlock from './PageBlock';
 function Home() {
   return (
     <div className="App-Home pattern1">
+      <div className="flex-Home">
+        <div className="flex-pageblock">
+          <PageBlock
+            title={'Hair'}
+            semantic={'woman with curly hair in front of α trumpet vine fence'}
+            description={'In Amadeus we see each face as a piece of art ready to be presented to the world'}
+          />
+        </div>
 
-      <PageBlock
-        title={'Hair'}
-        semantic={'woman with curly hair in front of α trumpet vine fence'}
-        description={'In Amadeus we see each face as a piece of art ready to be presented to the world'}
-      />
+        <div className="flex-pageblock">
+          <PageBlock
+            title={'Products'}
+            semantic={'Products we use'}
+            description={'We think...green'}
+          />
+        </div>
 
-      <PageBlock
-        title={'Products'}
-        semantic={'Products we use'}
-        description={'We think...green'}
-      />
+        <div className="flex-pageblock">
+          <PageBlock
+            title={'Cafe'}
+            semantic={'Music and coffee'}
+            description={'For our friends and guests that want to feel a taste of 80’s in their coffee'}
+          />
+        </div>
 
-      <PageBlock
-        title={'Cafe'}
-        semantic={'Music and coffee'}
-        description={'For our friends and guests that want to feel a taste of 80’s in their coffee'}
-      />
+        <div className="flex-pageblock">
+          <PageBlock
+            title={'History'}
+            semantic={'our history'}
+            description={'A vision becomes reality'}
+          />  
+        </div>
 
-      <PageBlock
-        title={'History'}
-        semantic={'our history'}
-        description={'A vision becomes reality'}
-      />
+        <div className="flex-pageblock">
+          <PageBlock
+            title={'Book'}
+            semantic={'book online'}
+            description={'Pick up the date, time and the hairdresser of your choice'}
+          />
+        </div>
 
-      <PageBlock
-        title={'Book'}
-        semantic={'book online'}
-        description={'Pick up the date, time and the hairdresser of your choice'}
-      />
-
-      <PageBlock
-        title={'Contact'}
-        semantic={'contact details'}
-        description={'Ask us, anything you want'}
-      />
-
+        <div className="flex-pageblock">
+          <PageBlock
+            title={'Contact'}
+            semantic={'contact details'}
+            description={'Ask us, anything you want'}
+          />
+        </div>
+      </div>
     </div>
   );
 }
