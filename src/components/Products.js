b2eda0751b7b610d164d27959afe5c4bb71a8a6d
kostasptr <kostas-ptr@hotmail.com>
2021-10-15 15:59:24 +0300

    breakpoints added

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,7 +5,7 @@ import eco from '../images/eco_friendly.png'
 function Products() {
   return (
     <div className="App-Products">
-
+      <div className="flex-page">
       <div className="introduction_container">
         <p>We love edgy hairstyles and bold fashion statements that is why when it
         comes to our products our main focus is organic.
@@ -17,37 +17,37 @@ function Products() {
       </div>
 
       <div className="products_container">
-        <div>
-          <p>Recyclable Products</p>
-          {/* <p>Our natural approach to ingredients
+        <div className="greenLine">
+          <p className="hover-target">Recyclable Products</p>
+          <p className="hide">Our natural approach to ingredients
           ensures a remarkable result for everyone with no impact to our
-        planet.</p> */}
+        planet.</p>
         </div>
 
-        <div>
-          <p>No Animal-Derived Ingredients</p>
-          {/* <p>We only use high
+        <div className="greenLine">
+        <p className="hover-target">No Animal-Derived Ingredients</p>
+          <p className="hide">We only use high
           quality products that are cruelty free and plant based delivered in
-        recyclable packaging.</p> */}
+        recyclable packaging.</p>
         </div>
 
-        <div>
-          <p>Formaldehyde-Free Keratin Treatments</p>
-          {/* <p>It is our belief that turning to clean and natural solutions
-        is not an option but an absolute necessity for our ecosystem’s survival.</p> */}
+        <div className="greenLine">
+        <p className="hover-target">Formaldehyde-Free Keratin Treatments</p>
+          <p className="hide">It is our belief that turning to clean and natural solutions
+        is not an option but an absolute necessity for our ecosystem’s survival.</p>
         </div>
       
-        <div>
-          <p>Ammonia-Free Hair Color</p>
-          {/* <p>We prefer organic options that promote healthy and shiny hair.</p> */}
+        <div className="greenLine">
+        <p className="hover-target">Ammonia-Free Hair Color</p>
+          <p className="hide">We prefer organic options that promote healthy and shiny hair.</p>
         </div>
 
-        <div>
-          <p>Negative Carbon Footprint Products</p>
-          {/* <p>Yep, amazing hair and a happy planet can be possible.</p> */}
+        <div className="greenLine">
+        <p className="hover-target">Negative Carbon Footprint Products</p>
+          <p className="hide">Yep, amazing hair and a happy planet can be possible.</p>
         </div>
       </div>
-
+    </div>
     </div>
   );
 }
